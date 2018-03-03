173c0f092bf01259482df49f5bb19b04140d2103
ahsonkhan <ahson_ahmedk@yahoo.com>
2018-03-03 10:48:49 -0800

    Add back netstandard1.3 propertygroup and ThreadPoolScheduler impl
    Add the missing file
    
    Fix impl of ThreadPoolScheduler Schedule<TState> for netstandard1.3

--- src/System.IO.Pipelines/src/System/IO/Pipelines/ThreadPoolScheduler.NetStandard.cs
+++ src/System.IO.Pipelines/src/System/IO/Pipelines/ThreadPoolScheduler.NetStandard.cs
@@ -2,16 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace System.IO.Pipelines
 {
     internal sealed class ThreadPoolScheduler : PipeScheduler
     {
         public override void Schedule<TState>(Action<TState> action, TState state)
         {
-            System.Threading.ThreadPool.QueueUserWorkItem(s =>
+            Threading.ThreadPool.QueueUserWorkItem(s =>
             {
                 ((ActionObjectAsWaitCallback<TState>)s).Run();
             }, 
