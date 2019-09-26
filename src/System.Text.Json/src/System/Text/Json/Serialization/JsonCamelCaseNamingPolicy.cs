35e7b9e1ac8780f02ab9ea383f8b0412fbdb82f4
Ahson Khan <ahkha@microsoft.com>
2019-09-26 00:55:06 -0700

    Add tests for surrogate chars.

--- src/System.Text.Json/src/System/Text/Json/Serialization/JsonCamelCaseNamingPolicy.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/JsonCamelCaseNamingPolicy.cs
@@ -2,12 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
+
 namespace System.Text.Json
 {
     internal sealed class JsonCamelCaseNamingPolicy : JsonNamingPolicy
     {
         public override string ConvertName(string name)
         {
+            // If the first character is a surrogate pair, char.IsUpper will be false,
+            // and we return the string unchanged
             if (string.IsNullOrEmpty(name) || !char.IsUpper(name[0]))
             {
                 return name;
@@ -39,6 +43,58 @@ namespace System.Text.Json
                 chars[i] = char.ToLowerInvariant(chars[i]);
             }
 
+            return new string(chars);
+        }
+
+        public string ConvertName_Custom1(string name)
+        {
+            if (string.IsNullOrEmpty(name) || !char.IsUpper(name[0]))
+            {
+                return name;
+            }
+
+            char[] chars = name.ToCharArray();
+
+            Debug.Assert(chars.Length > 0 && char.IsUpper(chars[0]));
+
+            chars[0] = char.ToLowerInvariant(chars[0]);
+
+            if (chars.Length > 1)
+            {
+                if (!char.IsUpper(chars[1]))
+                {
+                    goto Done;
+                }
+            }
+            else
+            {
+                goto Done;
+            }
+
+            Debug.Assert(chars.Length > 1 && char.IsUpper(chars[1]));
+
+            int lastCharIndex = chars.Length - 1;
+            for (int i = 1; i < lastCharIndex; i++)
+            {
+                char nextChar = chars[i + 1];
+                // Stop when next char is already lowercase.
+                if (!char.IsUpper(nextChar))
+                {
+                    // If the next char is a space, lowercase current char before exiting.
+                    if (nextChar == ' ')
+                    {
+                        chars[i] = char.ToLowerInvariant(chars[i]);
+                    }
+
+                    goto Done;
+                }
+
+                chars[i] = char.ToLowerInvariant(chars[i]);
+            }
+
+            chars[lastCharIndex] = char.ToLowerInvariant(chars[lastCharIndex]);
+
+        Done:
             return new string(chars);
         }
     }
--- src/System.Text.Json/tests/Serialization/Value.WriteTests.cs
+++ src/System.Text.Json/tests/Serialization/Value.WriteTests.cs
@@ -2,8 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Text.Encodings.Web;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Xunit;
 
 namespace System.Text.Json.Serialization.Tests
@@ -32,6 +34,175 @@ namespace System.Text.Json.Serialization.Tests
             string result = JsonSerializer.Serialize(test, options);
 
             Assert.Equal("{\"name\":\"\u6D4B\u8A6611\"}", result);
+        }
+
+        public class Temp
+        {
+            public Dictionary<string, string> Dictionary { get; set; }
+        }
+
+        [Theory]
+        //[InlineData("lowerCase")]
+        //[InlineData("UpperCase")]
+        //[InlineData("UPperCase")]
+        //[InlineData("lower Case")]
+        //[InlineData("loweR Case")]
+        //[InlineData("Upper Case")]
+        //[InlineData("UppeR Case")]
+        //[InlineData("lower case")]
+        //[InlineData("loweR case")]
+        //[InlineData("Upper case")]
+        //[InlineData("UppeR case")]
+        //[InlineData("lower cASe")]
+        //[InlineData("loweR cASe")]
+        //[InlineData("Upper cASe")]
+        //[InlineData("UppeR cASe")]
+        //[InlineData("UPper Case")]
+        //[InlineData("UPpeR Case")]
+        //[InlineData("UPper case")]
+        //[InlineData("UPpeR case")]
+        //[InlineData("UPper cASe")]
+        //[InlineData("UPpeR cASe")]
+        //[InlineData("IPhone")]
+        [InlineData("IPHone")]
+        //[InlineData("iPhone")]
+
+        //[InlineData("i")]
+        //[InlineData("I")]
+        //[InlineData("i ")]
+        //[InlineData("I ")]
+
+        //[InlineData("ii")]
+        //[InlineData("II")]
+        //[InlineData("iI")]
+        //[InlineData("Ii")]
+        //[InlineData("ii ")]
+        //[InlineData("II ")]
+        //[InlineData("iI ")]
+        //[InlineData("Ii ")]
+
+        //[InlineData("iii")]
+        //[InlineData("III")]
+        //[InlineData("iiI")]
+        //[InlineData("iIi")]
+        //[InlineData("Iii")]
+        //[InlineData("iII")]
+        //[InlineData("IIi")]
+        //[InlineData("IiI")]
+        //[InlineData("iii ")]
+        //[InlineData("III ")]
+        //[InlineData("iiI ")]
+        //[InlineData("iIi ")]
+        //[InlineData("Iii ")]
+        //[InlineData("iII ")]
+        //[InlineData("IIi ")]
+        //[InlineData("IiI ")]
+
+        //[InlineData("IPhone")]
+        //[InlineData("IPHone")]
+        //[InlineData("IPH one")]
+        //[InlineData("IPH One")]
+        //[InlineData("IPHONe")]
+        //[InlineData("IPHON e")]
+        //[InlineData("IPHON E")]
+        //[InlineData("IPHONE")]
+        public static void TestCasing(string baseString)
+        {
+            var testObject = new Temp
+            {
+                Dictionary = new Dictionary<string, string>
+                {
+                    { baseString, "hi" }
+                }
+            };
+
+            string json = JsonSerializer.Serialize<Temp>(testObject, new JsonSerializerOptions { DictionaryKeyPolicy = JsonNamingPolicy.CamelCase });
+            Console.WriteLine("STJ: " + json);
+
+            testObject = JsonSerializer.Deserialize<Temp>(json);
+            json = JsonSerializer.Serialize(testObject);
+            Console.WriteLine("STJ-round: " + json);
+
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            string expected = JsonConvert.SerializeObject(testObject, new JsonSerializerSettings { ContractResolver = contractResolver });
+            Console.WriteLine(expected);
+
+            char[] baseStringArray = baseString.ToCharArray();
+            char[] testCharArray = new char[baseStringArray.Length + 2];
+            for (int i = 0; i <= baseStringArray.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    testCharArray[j] = baseStringArray[j];
+                }
+                testCharArray[i] = '\uD835';
+                testCharArray[i + 1] = '\uDD80';
+                //testCharArray[i] = '\uD801';
+                //testCharArray[i + 1] = '\uDC27';
+                for (int j = i; j < baseStringArray.Length; j++)
+                {
+                    testCharArray[j + 2] = baseStringArray[j];
+                }
+
+                string testString = new string(testCharArray);
+
+                testObject = new Temp
+                {
+                    Dictionary = new Dictionary<string, string>
+                    {
+                        { testString, "hi" }
+                    }
+                };
+
+                json = JsonSerializer.Serialize<Temp>(testObject, new JsonSerializerOptions { DictionaryKeyPolicy = JsonNamingPolicy.CamelCase });
+                Console.WriteLine("STJ: " + json);
+
+                testObject = JsonSerializer.Deserialize<Temp>(json);
+                json = JsonSerializer.Serialize(testObject);
+                Console.WriteLine("STJ-round: " + json);
+
+                expected = JsonConvert.SerializeObject(testObject, new JsonSerializerSettings { ContractResolver = contractResolver });
+                Console.WriteLine(expected);
+            }
+
+            baseStringArray = baseString.ToCharArray();
+            testCharArray = new char[baseStringArray.Length + 1];
+            for (int i = 0; i <= baseStringArray.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    testCharArray[j] = baseStringArray[j];
+                }
+                testCharArray[i] = '\uD835';
+                for (int j = i; j < baseStringArray.Length; j++)
+                {
+                    testCharArray[j + 1] = baseStringArray[j];
+                }
+
+                string testString = new string(testCharArray);
+
+                testObject = new Temp
+                {
+                    Dictionary = new Dictionary<string, string>
+                    {
+                        { testString, "hi" }
+                    }
+                };
+
+                json = JsonSerializer.Serialize<Temp>(testObject, new JsonSerializerOptions { DictionaryKeyPolicy = JsonNamingPolicy.CamelCase });
+                Console.WriteLine("STJ: " + json);
+
+                testObject = JsonSerializer.Deserialize<Temp>(json);
+                json = JsonSerializer.Serialize(testObject);
+                Console.WriteLine("STJ-round: " + json);
+
+                expected = JsonConvert.SerializeObject(testObject, new JsonSerializerSettings { ContractResolver = contractResolver });
+                Console.WriteLine(expected);
+            }
         }
 
         [Fact]
